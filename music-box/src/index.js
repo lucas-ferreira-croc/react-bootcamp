4b92096908b092553fa4720925a3faeed8dc339b
Lucas Ferreira <lucas.ferreira057@bandtec.com.br>
2021-04-11 20:58:29 -0300

    pages

--- music-box/src/index.js
+++ music-box/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import NavBar from './components/NavBar';
-import HomePage from './components/HomePage';
+//import NavBar from './components/NavBar';
+//import HomePage from './components/HomePage';
 
 import '../src/assets/2021-1_assets_bootcamp-react_music-box/css/reset.css'
 import './assets/2021-1_assets_bootcamp-react_music-box/css/style.css';
+import Inicial from './pages/Inicial';
+import MinhasMusicas from './pages/MinhasMusicas';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <NavBar />
-    <HomePage />
+    <MinhasMusicas/>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ music-box/src/pages/Inicial.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+import HomePage from '../components/HomePage';
+
+function Inicial(){
+    return(
+        <>
+            <NavBar />
+            <Homepage/>
+        </>   
+    );
+}
+
+export default Inicial;
